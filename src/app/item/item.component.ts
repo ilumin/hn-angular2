40b6c88a88d73c7cb7f1e306dd264889699aae19
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-24 17:20:34 +0700

    UPDATE item template and styles

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { HackernewsApiService } from '../hackernews-api.service';
+
 @Component({
   selector: 'item',
   templateUrl: './item.component.html',
@@ -7,10 +9,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   @Input() itemID: number;
+  item;
 
-  constructor() { }
+  constructor(private _service: HackernewsApiService) { }
 
   ngOnInit() {
+    this._service.fetchItem(this.itemID)
+      .subscribe(
+        data => this.item = data,
+        error => console.error('Could not load item' + this.itemID)
+      );
   }
 
 }
