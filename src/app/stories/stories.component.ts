1db3d23e9f84711958668e70a8ae3faa052e2aef
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-24 23:31:17 +0700

    UPDATE stories component implement route

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 
 import { HackernewsApiService } from '../hackernews-api.service';
 
@@ -9,17 +10,32 @@ import { HackernewsApiService } from '../hackernews-api.service';
   styleUrls: ['./stories.component.scss']
 })
 export class StoriesComponent implements OnInit {
-  items: number[];
+  items;
+  typeSub: any;
+  pageSub: any;
+  storiesType: string;
+  pageNum: number;
+  listStart: number;
 
-  constructor(private _service: HackernewsApiService) {
-  }
+  constructor(
+    private _service: HackernewsApiService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this._service.fetchStories('news', 1)
-      .subscribe(
-        items => this.items = items,
-        error => console.error('Error fetching stories.')
-      );
+    this.typeSub = this.route.data
+      .subscribe(data => this.storiesType = data['storiesType']);
+
+    this.pageSub = this.route.params
+      .subscribe(params => {
+        this.pageNum = +params['page'] ? +params['page'] : 1;
+        this._service.fetchStories(this.storiesType, this.pageNum)
+          .subscribe(
+            items => this.items = items,
+            error => console.error('Error fetching ' + this.storiesType + ' stories.'),
+            () => this.listStart = ((this.pageNum - 1) * 30) + 1
+          );
+      });
   }
 
 }
