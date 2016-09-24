f5e4b8833dd3deb1569d147fa08cb9bd20d8d5d2
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-24 16:18:41 +0700

    ADD stories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StoriesComponent
   ],
   imports: [
     BrowserModule,
