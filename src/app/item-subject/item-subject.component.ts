d07ddc2f9240dfeb6eb16e297b1f8f404ada15c4
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-25 11:37:11 +0700

    UPDATE item subject logic

--- src/app/item-subject/item-subject.component.ts
+++ src/app/item-subject/item-subject.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-subject',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item-subject.component.scss']
 })
 export class ItemSubjectComponent implements OnInit {
+  @Input() item;
 
   constructor() { }
 
