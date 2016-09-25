65afa9b5bb1f6b95bc33147650e6c4adb3ebb76c
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-25 11:27:12 +0700

    ADD comment link component

---
+++ src/app/comment-link/comment-link.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CommentLinkComponent } from './comment-link.component';
+
+describe('Component: CommentLink', () => {
+  it('should create an instance', () => {
+    let component = new CommentLinkComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/comment-link/comment-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-link',
+  templateUrl: './comment-link.component.html',
+  styleUrls: ['./comment-link.component.scss']
+})
+export class CommentLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
