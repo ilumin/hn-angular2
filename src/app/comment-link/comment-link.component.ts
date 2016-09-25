3322da1e5039a6842c5d7a3968fb5024e4e6afd4
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-09-25 11:28:46 +0700

    UPDATE comment link data

--- src/app/comment-link/comment-link.component.ts
+++ src/app/comment-link/comment-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comment-link',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-link.component.scss']
 })
 export class CommentLinkComponent implements OnInit {
+  @Input() item;
 
   constructor() { }
 
